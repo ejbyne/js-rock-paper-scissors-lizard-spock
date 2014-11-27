9bb6e727dd0cc4871b3dbf15bec887a3fa98efb2
ed <ejbyne@gmail.com>
2014-11-27 16:58:57 +0000

    added form for name

--- public/javascript/PageInteraction.js
+++ public/javascript/PageInteraction.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
 
-  var welcomeMessage = function(){
-    $('#welcome').html('You are ' + human.name);
+  var startGame = function(){
+    $('#start').click(function() {
+      enterName();
+      lowerFromTop();
+      welcomeMessage();
+    });
+  };
+
+  var enterName = function(){
+    event.preventDefault();
+    var name = $('#text').val();
+    human.setName(name)
   };
 
   var lowerFromTop = function(){
     $('article').animate({top: '+=350px'}, 1500);
+  };
+
+  var welcomeMessage = function(){
+    $('#welcome').html('You are ' + human.name);
+  };
+
+
+  var makePicks = function(){
+    $('#choices a').on('click', function(event){
+      bounce(this);
+      recordPicks(this);
+      printResults();
+    });
   };
 
   var bounce = function(item){
@@ -13,43 +36,32 @@ $(document).ready(function(){
     $(item).effect("bounce", {times: 5}, 1000);
   };
 
-  var runGame = function(){
-    $('#choices a').on('click', function(event){
-      bounce(this);
-      makePicks(this);
-      printResults();
-    });
-  };
-
-  var makePicks = function(item){
+  var recordPicks = function(item){
     var picked = $(item).data('pick');
     human.picks(picked);
     computer.picks();
   };
   
+  var printResults = function() {
+    $(render(game)).prependTo('#results').slideDown();
+    $('li:nth-child(5)').fadeOut(3000, remove);    
+  };
+
   var render = function(game){
     var html = $('#list-item-template').html();
     var data = {message: game.victoryMessage()};
     return Mustache.render(html, data);
   };
 
-  var printResults = function() {
-    $(render(game)).prependTo('#results').slideDown();
-    $('li:nth-child(5)').fadeOut(3000, remove);    
-  };
-
   var remove = function(){
     $(this).remove();
   };
   
+  var human = new Player();
   var computer = new AutoPlayer('Computer');
-  
-  var human = new Player('Chris');
-  
   var game = new Game(computer, human);
-  
-  lowerFromTop();
-  welcomeMessage();
-  runGame();
+
+  startGame();
+  makePicks();
   
 })
--- public/javascript/Player.js
+++ public/javascript/Player.js
@@ -1,5 +1,6 @@
 function Player() {
-  this.name = prompt("Please enter your name");
+  // this.name = name
+  // this.name = prompt("Please enter your name");
   // this.history = [];
 };
 
@@ -7,3 +8,7 @@ Player.prototype.picks = function(pick) {
   this.pick = pick
   // this.history.push(pick);
 };
+
+Player.prototype.setName = function(name) {
+  this.name = name
+}
